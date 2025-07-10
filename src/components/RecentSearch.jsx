90219c305241fdd0672d0b9483dfbc20ecdcb9e6
20div <srivastavadivya047@gmail.com>
2025-07-10 20:02:24 +0530

    Initial commit

---
+++ src/components/RecentSearch.jsx
@@ -0,0 +1,47 @@
+function RecentSearch({ recentHistory, setRecentHistory, setselectedHistory }) {
+
+    const clearHistory = () => {
+        localStorage.clear();
+        setRecentHistory([])
+    }
+
+    const clearselectedHistory=(selectedItem)=>{
+        let history = JSON.parse(localStorage.getItem('history'));
+        console.log(history);
+         history=history.filter((item)=>{
+            if(item!=selectedItem){
+                 return item
+            }
+        })
+        setRecentHistory(history)
+        localStorage.setItem('history' , JSON.stringify(history));
+        console.log(history);
+        
+ } 
+
+
+    return (
+        <>
+            <div className='col-span-1  dark:bg-zinc-800 bg-red-100 pt-3'>
+                <h1 className='text-xl dark:text-white text-zinc-800 flex text-center justify-center' >
+                    <span>Recent Search</span>
+                    <button onClick={clearHistory} className='cursor-pointer'><svg xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" /></svg></button>
+                </h1>
+                <ul className='text-left overflow-auto mt-2'>
+                    {
+                        recentHistory && recentHistory.map((item, index) => (
+                            <div className="flex justify-between  pr-3 py-1">
+                            <li key={index} onClick={() => setselectedHistory(item)} className=' w-full pl-5 px-5 truncate dark:text-zinc-400  text-zinc-700 cursor-pointer dark:hover:bg-zinc-700 dark:hover:text-zinc-200 hover:bg-red-200 hover:text-zinc-800'>{item}</li>
+                                <button onClick={()=>clearselectedHistory(item)} className='cursor-pointer hover:bg-zinc-900 bg-zinc-700'><svg xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" /></svg></button>
+                            </div>
+                        ))
+                    }
+                </ul>
+            </div>
+        </>
+
+    )
+
+}
+
+export default RecentSearch
